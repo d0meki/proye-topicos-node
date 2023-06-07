5990d87d44940b6ef5ae587fc7095641188e2d07
d0meki <domeki151190@gmail.com>
2023-06-07 16:05:11 -0400

    reclamo controller

--- controllers/reclamoController.js
+++ controllers/reclamoController.js
@@ -1,1 +1,15 @@
 const { response, request } = require('express')
+
+
+async function getREclamos(req, res) {
+
+}
+async function getReclamoPorCategoria(req, res) {
+
+}
+async function getReclamoPorEstado(req, res) {
+
+}
+async function getReclamoPorFEcha(req, res) {
+
+}
